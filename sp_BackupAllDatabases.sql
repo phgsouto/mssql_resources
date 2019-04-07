d86d53731e8afc4d9d1ac5dab9a4e84d4c162026
Pedro H. G. Souto <phgsouto@gmail.com>
2019-04-07 12:15:22 -0300

    First commit. Adding sp_BackupAllDatabases.

---
+++ sp_BackupAllDatabases.sql
@@ -0,0 +1,74 @@
+USE [master]
+GO
+
+/**
+ * Script: sp_BackupAllDatabases.sql
+ * 
+ * Purpose: Provides an easy way to perform a full backup of all databases of 
+ *          the instance.
+ * 
+ * Parameters and flags (all parameters are optional):
+ *      @_bkpPath: The location to save the backups, with a trailing backslash.
+ *                 Default value: 'C:\Temp\'
+ *      @_ignoreTempDB: Should the procedure ignore the TempDB?
+ *                      Default value: 1 (true)
+ *      @_bkpSystemDBs: Should the procedure back up the system databases?
+ *                      Default value: 1 (true)
+ *                      Obs: This flag does not override @_ignoreTempDB.
+ *
+ */
+
+CREATE OR ALTER PROCEDURE dbo.sp_BackupAllDatabases
+    @_bkpPath AS NVARCHAR(MAX) = N'C:\Temp\'
+   ,@_ignoreTempDB AS BIT = 1 
+   ,@_bkpSystemDBs AS BIT = 1
+AS BEGIN
+    SET NOCOUNT ON
+
+    -- Checks for trailing baskslash on @_bkpPath
+    IF RIGHT(@_bkpPath, 1) <> '\'
+        SET @_bkpPath = @_bkpPath + '\'
+
+    -- Checks if @_bkpPath exists
+    DECLARE @_bkpPathExists AS INT
+    SELECT @_bkpPathExists=file_is_a_directory 
+      FROM sys.dm_os_file_exists(@_bkpPath);
+    IF @_bkpPathExists = 0 
+    BEGIN
+        PRINT 'The backup path does not exist. Create it or provide a valid one.'
+        RETURN
+    END
+
+    -- Creates temp table to store information about the databases and backups
+    DROP TABLE IF EXISTS #_databases
+
+    SELECT [name], 0 AS bkpd INTO #_databases FROM sys.databases
+
+    IF @_ignoreTempDB = 1
+        DELETE #_databases WHERE [name] = 'TempDB'
+
+    IF @_bkpSystemDBs = 0
+        DELETE #_databases WHERE [name] in ('master','model','msdb')
+
+    -- Start backing up the databases
+    DECLARE @_remaining AS INT
+    SELECT @_remaining=count(*) FROM #_databases WHERE bkpd = 0
+    WHILE @_remaining > 0
+    BEGIN
+        DECLARE @_db NVARCHAR(MAX)
+        SELECT TOP 1 @_db = [name] FROM #_databases WHERE bkpd = 0
+        DECLARE @_bkpFileName AS NVARCHAR(MAX)
+        SET @_bkpFileName = @_bkpPath + @_db + '_full_' 
+                + replace(convert(varchar, GETDATE(), 126), ':', '') + '.bak'
+        
+        DECLARE @_sql AS NVARCHAR(MAX)
+        SET @_sql = 'BACKUP DATABASE ' + @_db + ' TO DISK=''' + @_bkpFileName 
+                + ''' WITH COMPRESSION, CHECKSUM'
+        
+        EXEC sp_executesql @_sql
+
+        UPDATE #_databases SET bkpd = 1 WHERE [name] = @_db
+        SELECT @_remaining=count(*) FROM #_databases WHERE bkpd = 0
+    END
+
+END
